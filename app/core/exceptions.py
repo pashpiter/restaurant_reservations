d4db3a1a9167bf6aaa098d14358dbd04d5d79996
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-10 18:46:15 +0300

    exceptions fix

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -5,9 +5,9 @@ from fastapi import HTTPException
 
 class NoTables(HTTPException):
     def __init__(self, message: str):
-        super().__init__(HTTPStatus.NO_CONTENT, message)
+        super().__init__(HTTPStatus.BAD_REQUEST, message)
 
 
 class NoObjWithId(HTTPException):
     def __init__(self, message: str):
-        super().__init__(HTTPStatus.NO_CONTENT, message)
+        super().__init__(HTTPStatus.BAD_REQUEST, message)
