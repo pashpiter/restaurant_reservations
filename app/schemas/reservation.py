e4c4d8f7b28712a83712ae51d227dfaf5cbfd433
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-10 15:22:33 +0300

    reservation model

---
+++ app/schemas/reservation.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from sqlmodel import Field, SQLModel, Relationship
+
+from schemas.table import Table
+
+
+class ReservationBase(SQLModel):
+    customer_name: str = Field(
+        description='Имя заказчика'
+    )
+    reservation_time: datetime = Field(
+        description='Время начала брони'
+    )
+    duration_minutes: int = Field(
+        description='Продолжительность брони в минутах'
+    )
+
+
+class Reservation(ReservationBase):
+    id: int | None = Field(default=None, primary_key=True)
+    table_id: int = Field(foreign_key="table.id")
+
+    table: Table = Relationship(sa_relationship_kwargs={'lazy': 'joined'})
+
+
+class ReservationCreate(ReservationBase):
+    pass
+
+
+class ReservationRead(ReservationBase):
+    pass
