09b9884f99f1dc8e32d7c4c91429f1c6acd84c25
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-10 15:22:22 +0300

    table model

---
+++ app/schemas/table.py
@@ -0,0 +1,27 @@
+from sqlmodel import Field, SQLModel
+
+
+class TableBase(SQLModel):
+    name: str = Field(
+        description='Название стола',
+        schema_extra={'example': 'Table 1'}
+    )
+    seats: int = Field(
+        description='Количество мест за столом'
+    )
+    location: str = Field(
+        description='Располодение стола',
+        schema_extra={'example': 'Зал у окна'}
+    )
+
+
+class Table(TableBase):
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class TableCreate(TableBase):
+    pass
+
+
+class TableRead(TableBase):
+    pass
