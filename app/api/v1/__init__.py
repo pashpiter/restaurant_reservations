b68902de03360e585be666d0360211a2c91abc91
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-11 18:17:29 +0300

    tags for routers

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from api.v1.endpoints.reservation import router as reservation_router
 from api.v1.endpoints.table import router as table_router
 
-router = APIRouter()
-router.include_router(reservation_router)
-router.include_router(table_router)
+router = APIRouter(prefix='/v1')
+router.include_router(reservation_router, tags=['Бронирование'])
+router.include_router(table_router, tags=['Столы'])
