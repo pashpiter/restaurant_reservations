a5732c79210134e8c2271e8bd7ca32872aaa68bf
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-11 16:14:51 +0300

    reservation conflict exeption

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -16,3 +16,8 @@ class NoObjWithId(HTTPException):
 class NoReservations(HTTPException):
     def __init__(self, message: str):
         super().__init__(HTTPStatus.BAD_REQUEST, message)
+
+
+class ReservationConflict(HTTPException):
+    def __init__(self, message: str):
+        super().__init__(HTTPStatus.BAD_REQUEST, message)
