8ded5e7a191876eacadaab7a8e30c69f5160fe88
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-11 16:15:52 +0300

    check reservation conflict util

---
+++ app/api/v1/utils.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from schemas.reservation import ReservationCreate
+
+from db.crud.reservation import reservation_crud
+from api.v1.validators import is_reservation_conflict
+
+
+async def check_conflicts(
+        session: AsyncSession, new_reservation: ReservationCreate
+) -> None:
+    start_time = new_reservation.reservation_time
+    end_time = start_time + timedelta(
+        minutes=new_reservation.duration_minutes
+    )
+    conflicts = await reservation_crud.find_conflicting_reservations(
+        session, start_time, end_time, new_reservation.table_id
+    )
+    await is_reservation_conflict(conflicts)
