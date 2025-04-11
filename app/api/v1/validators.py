36e9569605c46603bba5825069056976ca99d74d
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-11 16:15:13 +0300

    reservation conflict validator

--- app/api/v1/validators.py
+++ app/api/v1/validators.py
@@ -1,16 +1,33 @@
-from core.exceptions import NoTables, NoReservations
+from datetime import timedelta
+
+from core.exceptions import NoTables, NoReservations, ReservationConflict
 from schemas.reservation import Reservation
 from schemas.table import Table
 
 NO_TABLES = 'Нет ни одного стола'
 NO_RESERVATIONS = 'Нет ни одной брони'
+RESERVATATION_CONFLICT = 'Cтолик занят c {} до {}'
 
 
-async def is_one_table(tables: list[Table]) -> None:
+async def is_one_table(tables: list[Table | None]) -> None:
     if not tables:
         raise NoTables(NO_TABLES)
 
 
-async def is_one_reservation(reservations: list[Reservation]) -> None:
+async def is_one_reservation(reservations: list[Reservation | None]) -> None:
     if not reservations:
         raise NoReservations(NO_RESERVATIONS)
+
+
+async def is_reservation_conflict(
+        reservations: list[Reservation | None]
+) -> None:
+    if reservations:
+        raise ReservationConflict(
+            [
+                RESERVATATION_CONFLICT.format(
+                    r.reservation_time,
+                    r.reservation_time + timedelta(minutes=r.duration_minutes)
+                ) for r in reservations
+            ]
+        )
