de62ee18501b846630c18c38046ac8296d644675
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-14 14:09:12 +0300

    reservation tests

--- tests/fixtures/reservations.py
+++ tests/fixtures/reservations.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
--- tests/test_reservations.py
+++ tests/test_reservations.py
@@ -25,7 +25,9 @@ async def test_reservation(client: AsyncClient, test_reservation: Reservation):
 async def test_create_reservation(client: AsyncClient, test_table_one: Table):
     input_data = {
         'customer_name': 'Dwayne Johnson',
-        'reservation_time': datetime.now().isoformat(),
+        'reservation_time': (
+            datetime.now() + timedelta(minutes=2)
+        ).isoformat(),
         'duration_minutes': 30,
         'table_id': test_table_one.id
         }
@@ -40,3 +42,33 @@ async def test_create_reservation(client: AsyncClient, test_table_one: Table):
         'duration_minutes'] == input_data['duration_minutes']
     assert created_reservation[
         'table_id'] == input_data['table_id']
+
+
+@pytest.mark.asyncio
+async def test_create_crossed_reservation(
+        client: AsyncClient,
+        test_table_one: Table,
+        test_reservation: Reservation
+):
+    input_data = {
+        'customer_name': 'Dwayne Johnson',
+        'reservation_time': (
+            datetime.now() + timedelta(minutes=2)
+        ).isoformat(),
+        'duration_minutes': 30,
+        'table_id': test_table_one.id
+        }
+    response = await client.post('/v1/reservations/', json=input_data)
+    assert response.status_code == 400
+    data = response.json()
+    assert data['detail'][0].startswith('Cтолик занят')
+
+
+@pytest.mark.asyncio
+async def test_delete_reservation(
+    client: AsyncClient,
+    test_reservation: Reservation
+):
+    response = await client.delete(f'/v1/reservations/{test_reservation.id}')
+    assert response.status_code == 204
+    assert not response.content
